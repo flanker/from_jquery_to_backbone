cca9fc3c5d594b4a22bb36615a6a7debaab1a520
FENG Zhichao <flankerfc@gmail.com>
2012-08-02 23:53:02 +1000

    start the application after load templates complete

--- public/js/using_backbone/app_using_backbone.js
+++ public/js/using_backbone/app_using_backbone.js
@@ -1,23 +1,6 @@
-var App = {};
-
-App.views = {};
-App.models = {};
-
-var templates = [
-  'tweets'
-];
-
 $(function () {
-  $(templates).each(function (index, name) {
-    $.get('/js/using_backbone/templates/' + name + '-template.html.haml', function (template) {
-      var templateElement = $('<script type="text/haml-template" id="' + name + '-template"></script>');
-      templateElement.html(template).appendTo($('#haml-templates'));
-      init();
-    });
+  App.helper.loadTemplates(function () {
+    var tweetsView = new App.views.TweetsView({model: {tweet: 'hello', created_at: 'test'}});
+    $('.main-container').html(tweetsView.el);
   });
 });
-
-var init = function () {
-  var tweetsView = new App.views.TweetsView({model: {tweet: 'hello', created_at: 'test'}});
-  $('.main-container').html(tweetsView.el);
-};
---
+++ public/js/using_backbone/base_app.js
@@ -0,0 +1,33 @@
+var App = {};
+
+App.views = {};
+App.models = {};
+App.helper = {};
+App.config = {};
+
+App.config.templates = [
+  'tweets'
+];
+
+App.helper.loadTemplates = function (callback) {
+  var loadedTemplates = 0;
+
+  $(App.config.templates).each(function (index, name) {
+    $.get('/js/using_backbone/templates/' + name + '-template.html.haml', function (template) {
+      registerTemplate(name, template);
+      loadedTemplates ++;
+      if (isLoadComplete()) {
+        callback();
+      }
+    });
+  });
+
+  var registerTemplate = function (name, template) {
+    var templateElement = $('<script type="text/haml-template" id="' + name + '-template"></script>');
+    templateElement.html(template).appendTo($('#haml-templates'));
+  };
+
+  var isLoadComplete = function () {
+    return loadedTemplates === App.config.templates.length;
+  };
+};
